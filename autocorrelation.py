13364137c77373e9fcfd28b84b3fb5d47e1991a5
Konda <dikonda@apac.corpdir.net>
2017-11-02 01:41:28 +0530

    Second Commit

--- autocorrelation.py
+++ autocorrelation.py
@@ -30,45 +30,94 @@ Data = pd.read_json(filename)
 # LSOA Names
 LSOA_Names = list(Data)
 
+print('Generating Files')
+
 # Dates as index from september 2014 to August 2017
 dates = ["2014-09-01", "2017-09-01"]
 start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in dates]
 Dates = OrderedDict(((start + timedelta(_)).strftime(r"%b-%y"), None) for _ in xrange((end - start).days)).keys()
+
+Correlation = pd.DataFrame()
+Correlation_Rank= pd.DataFrame()
 
 # Loop runs for each LSOA name
 for x in range(len(LSOA_Names)):
     Crime_Data = pd.DataFrame(Data[LSOA_Names[x]].values.tolist(), columns=Dates)
     Crime_Data.index=Data.index
     Crime_Data = Crime_Data.transpose()
-    Auto_Correlation = pd.DataFrame(index = Crime_Data.index, columns = Crime_Data.columns)
-    Auto_Correlation_rank = pd.DataFrame()
-    
-    for i in range(len(Crime_Data.columns)):         # Loop for each crime type         
-        for j in range(len(Crime_Data.index)):       # Loop for each time shift (shift in each month)
-            Auto_Correlation.values[j,i] = Crime_Data[Crime_Data.columns[i]].corr(Crime_Data[Crime_Data.columns[i]].shift(j).fillna(0))  # Auto-correlation function 
-        Auto_Correlation_rank = pd.concat([Auto_Correlation.rank(),Auto_Correlation_rank],axis=1)                                        # Ranking Auto-correlation values for each crime type along time shifts                                   
-    Auto_Correlation.index = range(len(Crime_Data.index))
-    
-    Auto_Correlation.to_csv('Auto_Correlation_%s' % LSOA_Names[x] + '.csv')   # Auto-correlation coefficients
-    Auto_Correlation_rank.to_csv('Auto_Correlation_Rank_%s' % LSOA_Names[x] + '.csv')   # rank of auto correlation values
-    
-    
-    Cross_Correlation = pd.DataFrame(index = Crime_Data.columns, columns = Crime_Data.columns)
-    Cross_Correlation_rank = pd.DataFrame(index = Crime_Data.columns, columns = Crime_Data.columns)
+    data_corr = pd.DataFrame(index = Crime_Data.columns, columns = Crime_Data.columns)
+    data_corr_rank = pd.DataFrame(index = Crime_Data.columns, columns = Crime_Data.columns)
   
     for i in range(len(Crime_Data.columns)):                              # Loop for pairwise combination of crime types  
-        for j in range(len(Crime_Data.columns)):                          
+        for j in range(len(Crime_Data.columns)):  
             for k in range(len(Crime_Data.index)):                            # Loop for each time shift
                 if(k == 0):
-                    Cross_Correlation_array = np.array(Crime_Data[Crime_Data.columns[i]].corr(Crime_Data[Crime_Data.columns[j]].shift(k).fillna(0)))    # Cross-correlation between each pair of crime type with time shift
+                    data_corr_array = np.array(Crime_Data[Crime_Data.columns[i]].corr(Crime_Data[Crime_Data.columns[j]].shift(k).fillna(0)))    # Cross-correlation between each pair of crime type with time shift
                 else:
-                    Cross_Correlation_array = np.append(np.array(Crime_Data[Crime_Data.columns[i]].corr(Crime_Data[Crime_Data.columns[j]].shift(k).fillna(0),method="spearman")),Cross_Correlation_array)   # Cross-correlation between each pair of crime type with time shift
-            Cross_Correlation_rank.values[i,j] = rankdata(Cross_Correlation_array)
-            Cross_Correlation.values[i,j] = Cross_Correlation_array
+                    data_corr_array = np.append(np.array(Crime_Data[Crime_Data.columns[i]].corr(Crime_Data[Crime_Data.columns[j]].shift(k).fillna(0))),data_corr_array)   # Cross-correlation between each pair of crime type with time shift
+            data_corr_rank.values[i,j] = rankdata(data_corr_array)
+            data_corr.values[i,j] = data_corr_array
+    
+    data_corr['Crime type 1'] = data_corr.index
+    data_corr = pd.melt(data_corr, id_vars='Crime type 1', value_name=LSOA_Names[x], var_name='Crime type 2')
+    data_corr_rank['Crime type 1'] = data_corr_rank.index
+    data_corr_rank = pd.melt(data_corr_rank, id_vars='Crime type 1', value_name=LSOA_Names[x], var_name='Crime type 2')
+    #Correlation: correlation matrix with all possible combinations, Correlation_Rank: Rank of correlation matrix
+    if (x == 0):
+        Correlation = data_corr
+        Correlation_Rank = data_corr_rank
 
-    Cross_Correlation.to_csv('Cross_Correlation_%s' % LSOA_Names[x] + '.csv')                        # Cross-correlation coefficients
-    Cross_Correlation_rank.to_csv('Cross_Correlation_Rank_%s' % LSOA_Names[x] + '.csv')              # Rank Cross-correlation values
+    else:
+        Correlation = Correlation.merge(data_corr)
+        Correlation_Rank = Correlation_Rank.merge(data_corr_rank)
+        
+# converting Rank of correlation matrix to JSON file format
+Correlation_Rank_Map = Correlation_Rank[Correlation_Rank['Crime type 1'] == Correlation_Rank['Crime type 2']]
+Correlation_Rank_Map.index = Correlation_Rank_Map['Crime type 1']
+Correlation_Rank_Map = Correlation_Rank_Map.drop(Correlation_Rank_Map.columns[[0, 1]],axis=1)
+Correlation_Rank_Map = Correlation_Rank_Map.to_json(orient='columns')
+
+with open("Correlation_Rank_Map.txt", "w") as text_file:
+    text_file.write(Correlation_Rank_Map)
+    
+# To divide the array in each cell to individual value by adding'Time_Shift' column
+Correlation = explode(Correlation, LSOA_Names, fill_value='')
+Correlation['Time_Shift'] =  (range(len(Dates)) * len(Correlation))[0:len(Correlation)]
+
+Correlation.to_csv('Correlation.csv')
+
+# Getting highly correlated patterns
+Correlation_sort = pd.melt(Correlation, id_vars=['Crime type 1','Crime type 2','Time_Shift'], value_vars=LSOA_Names,var_name='LSOA Name', value_name='correlation').sort_values('correlation',ascending=False)
+High_Correlation = Correlation_sort[Correlation_sort['correlation']>0]
+
+High_Correlation.to_csv('Top_Correlation.csv')
 
 
+# Function to convert array in each cell to individual value
+def explode(df, lst_cols, fill_value=''):
+    # make sure `lst_cols` is a list
+    if lst_cols and not isinstance(lst_cols, list):
+        lst_cols = [lst_cols]
+    # all columns except `lst_cols`
+    idx_cols = df.columns.difference(lst_cols)
 
+    # calculate lengths of lists
+    lens = df[lst_cols[0]].str.len()
 
+    if (lens > 0).all():
+        # ALL lists in cells aren't empty
+        return pd.DataFrame({
+            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
+            for col in idx_cols
+        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
+          .loc[:, df.columns]
+    else:
+        # at least one list in cells is empty
+        return pd.DataFrame({
+            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
+            for col in idx_cols
+        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
+          .append(df.loc[lens==0, idx_cols]).fillna(fill_value) \
+          .loc[:, df.columns]
+
+print('End of Code') 
